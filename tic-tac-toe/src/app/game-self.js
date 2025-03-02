4d1e792521654ef382a228092514f13c10ef9c4d
Seey215 <seey215@qq.com>
2025-03-03 01:25:11 +0800

    feat: Completed the Tic-Tac-Toe

---
+++ tic-tac-toe/src/app/game-self.js
@@ -0,0 +1,124 @@
+'use client';
+
+import { useState } from 'react';
+
+
+function Square({ value, onSquareClick }) { // 组件：首字母必须大写的函数
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  )
+}
+
+
+function GameSelf({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice(); // 创建一个新数组，并把原数组的元素复制到新数组中
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? 'Winner: ' + winner
+    : 'Next player: ' + (xIsNext ? 'X' : 'O');
+
+  const boardRows = [];
+  for (let i = 0; i < 3; i++) {
+    const rowSquares = [];
+    for (let j = 0; j < 3; j++) {
+      const idx = i * 3 + j;
+      rowSquares.push(
+        <Square key={idx} value={squares[idx]} onSquareClick={() => handleClick(idx)} />
+      );
+    }
+
+    boardRows.push(
+      <div className="board-row" key={i}>
+        {rowSquares}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className='status'>{status}</div>
+      {boardRows}
+    </>
+  )
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  })
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <GameSelf xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+
+}
---
+++ tic-tac-toe/src/app/page.js
@@ -0,0 +1,12 @@
+import './game.css';
+import Game from "./game";
+import GameSelf from './game-self';
+
+export default function Home() {
+  return (
+    <>
+      <Game />
+      <GameSelf />
+    </>
+  );
+}
